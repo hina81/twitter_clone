c74d6a791ddda28de25575e33a01d70f0b704865
EC2 Default User <ec2-user@ip-172-31-0-176.ap-northeast-1.compute.internal>
2024-08-19 04:37:47 +0000

    Activestrageのインストール

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,8 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  
+  config.active_job.queue_adapter = :inline
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
@@ -73,4 +75,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "2f514c43aa48468baa47409a70b85528.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
 end
